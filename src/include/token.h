e6bb95ce03f718140af46af1ab02df407520192b
Matthew Hill <contact@matthew-hill.net>
2024-02-29 22:14:21 +0000

    Adding lexer and token implementation for test.q example

---
+++ src/include/token.h
@@ -0,0 +1,22 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <stdlib.h>
+
+typedef struct TOKEN_STRUCT {
+  enum {
+    TOKEN_ID,
+    TOKEN_EQUALS,
+    TOKEN_STRING,
+    TOKEN_SEMI,
+    TOKEN_COLON,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN
+  } type;
+
+  char *value;
+
+} token_T;
+
+token_T *init_token(int type, char *value);
+
+#endif // !TOKEN_H
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "include/lexer.h"
+
 int main(int argc, char *arcv[]) {
-  printf("Hello World\n");
+  lexer_T* lexer = init_lexer(
+    "set string::name = \"matt\";\n"
+    "echo(name)"
+
+  );
+
+  token_T* token = (void*)0;
+
+  while ((token = lexer_get_next_token(lexer)) != (void*)0) {
+    printf("TOKEN(%d, %s)\n", token->type, token->value);
+  }
 
   return 0;
 }
